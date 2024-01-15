d393c36bd5ac1d208a1860b0dc5a1a198c7917ad
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-01-15 15:40:46 +0200

    Add User.js

---
+++ src/models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
