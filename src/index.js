d4811b721622cd27cd0d09ccd65aee9b8e4b802c
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-01-15 15:45:28 +0200

    Set app to use cors middleware and express.json and express.urlencoded

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const { initializeDatabase } = require('./config/database');
 const { PORT } = require('./config/env');
 
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 initializeDatabase()
     .then(() => {
