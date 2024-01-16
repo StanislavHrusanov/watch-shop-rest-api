4980a22c3a79c859500a45653e1e9f2d86e6b9ea
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-01-16 15:29:21 +0200

    Update errorMapper

--- src/utils/errorMapper.js
+++ src/utils/errorMapper.js
@@ -3,8 +3,8 @@ exports.mapErrors = (err) => {
         return err.join('\n');
     } else if (err.name == 'ValidationError') {
         return Object.values(err.errors).map(e => e.message).join('\n');
-    } else if (typeof err.message == 'string') {
-        return err.message;
+    } else if (typeof err == 'string') {
+        return err;
     } else {
         return 'Request error';
     }
