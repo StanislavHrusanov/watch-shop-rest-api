942377925b8eb69486f766cc29e9927d51ce51d9
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-01-16 11:51:11 +0200

    Add authService with createToken and regiser functions

---
+++ src/services/authService.js
@@ -0,0 +1,40 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const { jwtSign } = require('../utils/jwtPromises');
+const { SALT_ROUNDS, SECRET } = require('../config/env');
+
+exports.createToken = (user) => {
+    const payload = {
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        isAdmin: user.isAdmin
+    }
+
+    const options = { expiresIn: '2d' }
+
+
+    return jwtSign(payload, SECRET, options);
+}
+
+exports.register = async ({ username, firstName, lastName, password, email, address, phoneNumber }) => {
+    const user = await User.findOne({ username });
+
+    if (user) {
+        throw 'Това потребителско име вече съществува!';
+    }
+
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+
+    const createdUser = await User.create({
+        username,
+        firstName,
+        lastName,
+        password: hashedPassword,
+        email,
+        address,
+        phoneNumber
+    });
+
+    return this.createToken(createdUser);
+}
