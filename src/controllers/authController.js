d06c7ccb13b3a2ccf13eb2da2abe08cabea49c83
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-01-16 15:30:14 +0200

    Add functionality to login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,13 +6,41 @@ router.post('/register', async (req, res) => {
     const userData = req.body;
 
     try {
-        const token = await authService.register(userData);
-        res.status(201).json(token);
+        const userSession = await authService.register(userData);
+        res.status(201).json(userSession);
+
     } catch (err) {
         console.error(err.message);
         const error = mapErrors(err);
         res.status(400).json({ message: error });
     }
+});
+
+router.post('/login', async (req, res) => {
+    const userData = req.body;
+    userData.username = userData.username.trim();
+    userData.password = userData.password.trim();
+
+    try {
+        if (userData.username == '' || userData.password == '') {
+            throw ('Невалидно потребителско име или парола!');
+        }
+
+        const userSession = await authService.login(userData);
+        res.json(userSession);
+
+    } catch (err) {
+        const error = mapErrors(err);
+        console.error(error);
+        res.status(400).json({ message: error });
+    }
+});
+
+router.get('/logout', async (req, res) => {
+    if (req.user) {
+        req.user = null;
+    }
+    res.status(204).end();
 });
 
 module.exports = router;
