c9d615d554590ac4307c012c4d61ee5e4faa4ee9
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-01-17 14:36:15 +0200

    Update routes to use watchController at /watches

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 const routes = require('express').Router();
 const authController = require('./controllers/authController');
+const watchController = require('./controllers/watchController');
 
 routes.use('/auth', authController);
+routes.use('/watches', watchController);
 
 module.exports = routes;
