f4a9aea78a0fafbe9a3ce3b169f8368fce3655d4
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-01-24 11:50:55 +0200

    Add functions to get all watches and get watches by type

--- src/services/watchService.js
+++ src/services/watchService.js
@@ -1,3 +1,7 @@
 const Watch = require('../models/Watch');
 
 exports.create = (watch) => Watch.create(watch);
+
+exports.getAll = () => Watch.find().sort({ createdAt: -1 });
+
+exports.getWatchesByType = (type) => Watch.find({ type: type }).sort({ createdAt: -1 });
