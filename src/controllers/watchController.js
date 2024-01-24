74dd011a45842fe48f16618708489aedadd33fc3
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-01-24 11:53:06 +0200

    Add functionality to get all watches, all men's watches and all women's watches

--- src/controllers/watchController.js
+++ src/controllers/watchController.js
@@ -7,12 +7,47 @@ router.post('/', async (req, res) => {
 
     try {
         const createdWatch = await watchService.create(watch);
-        console.log(createdWatch);
         res.status(201).json(createdWatch);
     } catch (err) {
         const error = mapErrors(err);
         console.error(error);
         res.status(400).json({ message: error });
+    }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const watches = await watchService.getAll();
+        res.json(watches);
+
+    } catch (err) {
+        const error = mapErrors(err);
+        console.error(error);
+        res.json({ message: error });
+    }
+});
+
+router.get('/men', async (req, res) => {
+    try {
+        const watches = await watchService.getWatchesByType('Мъжки');
+        res.json(watches);
+
+    } catch (err) {
+        const error = mapErrors(err);
+        console.error(error);
+        res.json({ message: error });
+    }
+});
+
+router.get('/women', async (req, res) => {
+    try {
+        const watches = await watchService.getWatchesByType('Дамски');
+        res.json(watches);
+
+    } catch (err) {
+        const error = mapErrors(err);
+        console.error(error);
+        res.json({ message: error });
     }
 });
 
