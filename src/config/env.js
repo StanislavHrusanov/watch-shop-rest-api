ff896353ac9c420bf29a82604e38cb1f37db1dc8
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-02-07 10:49:50 +0200

    Change connection string

--- src/config/env.js
+++ src/config/env.js
@@ -1,4 +1,4 @@
 exports.PORT = 3000;
-exports.CONNECTION_STRING = 'mongodb://localhost:27017/watchShop';
+exports.CONNECTION_STRING = 'mongodb+srv://stanislavhrusanov:S9J1tTKyN6g8Q2w5@baroto87.o5rj3pt.mongodb.net/watchShop';
 exports.SECRET = 'dadadasd.321dak2e_sakda.dandanj';
 exports.SALT_ROUNDS = 10;
