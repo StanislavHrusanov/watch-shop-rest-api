376ce9cff03cf99fcf82de531e71a5d805ce1455
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-02-15 10:50:15 +0200

    Add Order.js

---
+++ src/models/Order.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+    status: {
+        type: String,
+        default: 'Pending'
+    },
+    buyer: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    },
+    items: [
+        {
+            title: {
+                type: String
+            },
+            price: {
+                type: Number
+            },
+            imageUrl: {
+                type: String
+            }
+        }
+    ],
+    totalPrice: {
+        type: Number
+    }
+}, { timestamps: true });
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
