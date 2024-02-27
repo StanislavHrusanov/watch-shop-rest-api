8eb26e3561155e33be73dda7f1d0cb1b783623db
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-02-27 11:26:14 +0200

    Add isNotAdmin routGuard

--- src/middlewares/routGuards.js
+++ src/middlewares/routGuards.js
@@ -6,3 +6,12 @@ exports.isAdmin = async (req, res, next) => {
         next();
     }
 }
+
+exports.isNotAdmin = async (req, res, next) => {
+
+    if (req.user?.isAdmin) {
+        res.status(403).json({ message: 'Няма достъп!' });
+    } else {
+        next();
+    }
+}
